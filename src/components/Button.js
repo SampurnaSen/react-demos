9d18437ebffd271364775f48bc37c6342c4ff000
Rosie Prom <rosalinda.prom@gmail.com>
2022-08-29 18:33:30 +1000

    theming solution - lesson 8

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react'
+import { ThemeContext } from './ThemeProvider'
+
+const SwitchButton = () => {
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.darkMode;
+
+    const onClick = () => {
+        theme.setDarkMode(!darkMode);
+    }
+
+    return (
+        <button
+            className={`button ${darkMode ? "button-dark" : "button-light"}`}
+            onClick={onClick}
+        >
+            {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+    )
+
+}
+
+export default SwitchButton
