c4c192eba50a3174751e289d24cf4adf42a68597
Rosie Prom <rosalinda.prom@gmail.com>
2022-08-29 19:40:38 +1000

    auth context demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useContext, useEffect, useState } from "react"
+import { ThemeContext } from './store/theme-context';
+import AuthContext from "./store/auth-context";
+import MainHeader from './components/MainHeader/MainHeader'
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
 import './App.css';
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.darkMode;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if(storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+       <div className="App">
+        <MainHeader onLogout={logoutHandler}/>
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler}/>}
+            {isLoggedIn && <Home onLogout={logoutHandler}/>}
+          </main>
+        </div>
+    </AuthContext.Provider>
+     
   );
 }
 
